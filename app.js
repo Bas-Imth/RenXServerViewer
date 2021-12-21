7de7645f1479d6a316c26104f09cf39354b9d323
Alexander Reen <alexanderreen@gmail.com>
2021-12-21 01:57:29 +0100

    fix repo order

--- app.js
+++ app.js
@@ -3,43 +3,43 @@ const fs = require("fs");
 var renxdata;
 
 // Loading the index file . html displayed to the client
-var server = http.createServer(function(req, res) {
-  fs.readFile("./index.html", "utf-8", function(error, content) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(content);
-  });
+var server = http.createServer(function (req, res) {
+    fs.readFile("./index.html", "utf-8", function (error, content) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+    });
 });
 
 function fetchData() {
-  const https = require("http");
-  const options = {
-    hostname: "brotherhoodofnod.net",
-    port: 8080,
-    path: "/api/server/?Game%20Version=latest",
-    method: "GET"
-  };
+    const https = require("http");
+    const options = {
+        hostname: "brotherhoodofnod.net",
+        port: 8080,
+        path: "/api/server/?Game%20Version=latest",
+        method: "GET",
+    };
 
-  var data = "";
+    var data = "";
 
-  const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`);
+    const req = https.request(options, (res) => {
+        console.log(`statusCode: ${res.statusCode}`);
 
-    res.on("data", d => {
-      data += d;
+        res.on("data", (d) => {
+            data += d;
+        });
+
+        res.on("end", () => {
+            data = JSON.parse(data);
+
+            fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+        });
     });
 
-    res.on("end", () => {
-      data = JSON.parse(data);
-
-      fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+    req.on("error", (error) => {
+        console.error(error);
     });
-  });
 
-  req.on("error", error => {
-    console.error(error);
-  });
-
-  req.end();
+    req.end();
 }
 
 fetchData();
@@ -50,23 +50,23 @@ setInterval(broadcastToSocket, 5000);
 const io = require("socket.io")(server);
 // When a client connects, we note it in the console
 io.sockets.on("connection", function (socket) {
-  console.log("Incoming: " + socket.handshake.address);
-  socket.emit("message", "You are connected!");
-  renxdata = JSON.parse(fs.readFileSync("data.json", "utf8"));
-  io.local.emit("renxData", renxdata);
+    console.log("Incoming: " + socket.handshake.address);
+    socket.emit("message", "You are connected!");
+    renxdata = JSON.parse(fs.readFileSync("data.json", "utf8"));
+    io.local.emit("renxData", renxdata);
 });
 
 function broadcastToSocket() {
-  renxdata = JSON.parse(fs.readFileSync("data.json", "utf8"));
-  io.local.emit("renxData", renxdata);
+    renxdata = JSON.parse(fs.readFileSync("data.json", "utf8"));
+    io.local.emit("renxData", renxdata);
 }
 
 var nstatic = require("node-static");
 
-var file = new(nstatic.Server)(__dirname);
+var file = new nstatic.Server(__dirname);
 
 http.createServer(function (req, res) {
-  file.serve(req, res);
+    file.serve(req, res);
 }).listen(8081);
 
 server.listen(80);
